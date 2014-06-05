de742b73ca075dfd6c3c3cd1317d5d5230f31535
Okeowo <okeowoaderemi@gmail.com>
2014-06-05 16:50:57 +0100

    Added Icon,and Controller for the Widget and dummy content

---
+++ Widget/Diary/Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package ImpressPages
+
+ *
+ */
+namespace Plugin\Diary\Widget\Diary;
+
+
+
+
+class Controller extends \Ip\WidgetController{
+
+
+    public function getTitle() {
+    	return __('Diary Notes', 'ipAdmin');
+
+    }
+
+}
---
+++ Widget/Diary/skin/default.php
@@ -0,0 +1,10 @@
+<div class="ipsContent">
+    <div class="col-lg-8 col-lg-offset-2">
+					<p><i class="fa fa-user"></i><ba>Stanley Stinson</ba></p>
+					<p><bd>January 18, 2014</bd></p>
+					<h4>The Amazing Spiderman</h4>
+					<p><b>Spider-Man</b> is a fictional character, a comic book superhero that appears in comic books published by Marvel Comics. Created by writer-editor Stan Lee and writer-artist Steve Ditko, he first appeared in Amazing Fantasy #15 (cover-dated Aug. 1962). </p>
+					<p>Lee and Ditko conceived the character as an orphan being raised by his Aunt May and Uncle Ben, and as a teenager, having to deal with the normal struggles of adolescence in addition to those of a costumed crimefighter.</p>
+					<p><a href="blog01.html">Continue Reading...</a></p>
+				</div>
+</div>
