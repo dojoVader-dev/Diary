bbbf80a2928c312da7a1b767843f78dfeb5e842f
Okeowo <okeowoaderemi@gmail.com>
2014-06-08 21:13:02 +0100

    View for Single Article

---
+++ view/_article.php
@@ -0,0 +1,10 @@
+
+  <div class="col-lg-8 col-lg-offset-2">
+    <h1><?php echo $title?></h1><hr/>
+
+					<p><bd>January 18, 2014</bd></p>
+
+					<p>
+					<?php echo $content?>
+					</p>
+</div>
