181fd715ddd184d33222e20bc1b1e7ba8d29d123
Okeowo <okeowoaderemi@gmail.com>
2014-06-08 21:12:26 +0100

    Added read code to display blog content

--- PublicController.php
+++ PublicController.php
@@ -7,6 +7,11 @@ class PublicController extends \Ip\Controller
     public function read()
     {
 
+		$article=new Model();
+		$id=(int)ipRequest()->getQuery('post');
+
+		$data=$article->getArticleById($id);
+		return ipView(__DIR__."/view/_article.php",$data)->render();
 
 
     }
