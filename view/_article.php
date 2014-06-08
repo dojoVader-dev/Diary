37d0dadd65263a6258b393af4992e6a94fe4fd09
Okeowo <okeowoaderemi@gmail.com>
2014-06-08 22:30:08 +0100

    Removed ipslot

--- view/_article.php
+++ view/_article.php
@@ -1,6 +1,6 @@
 
   <div class="col-lg-8 col-lg-offset-2">
-  <?php echo ipSlot('breadcrumb');?>
+
     <h1><?php echo $title?></h1><hr/>
 
 					<p><bd><?php echo date("M d,Y",strtotime($date)); ?></bd></p>
