a5a7bde15f932b64644fb132b89ceacee1e28831
Okeowo <okeowoaderemi@gmail.com>
2014-06-12 16:41:07 +0100

    Added Comment Controller to handle Comment Page Interactivity

---
+++ assets/js/CommentController.js
@@ -0,0 +1,26 @@
+define(['dijit/Dialog'],function(dialog) {
+	"use strict";
+    var dia = new dialog({
+        title: "Comment Page",
+        content: "Submitting Comment"
+    });
+    return {
+        init: function() {
+            //Catch the Response Data from the Request
+            jQuery(document).on("ipSubmitResponse", function(e, resp) {
+                console.log(resp.status);
+                if ("status" in resp) {
+                    switch (resp.status) {
+                        case "error":
+                        case "success":
+                            dia.set('content',resp.message);
+                            dia.set('title',"Comment Status")
+                            dia.show();
+                            
+                            break;
+                    }
+                }
+            });
+        }
+    }
+});
