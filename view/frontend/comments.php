e3ea72a96c20be8b2a37903d08753605d387e866
Okeowo <okeowoaderemi@gmail.com>
2014-06-16 16:17:39 +0100

    Changed the template for Comment better interface now

--- view/frontend/comments.php
+++ view/frontend/comments.php
@@ -5,13 +5,23 @@
 
 foreach ($data as $key=>$comment): 
 ?>
-<div>
-<small>Author (<?php echo $comment['author'] ?>) said:</small>
-<p><?php echo $comment['content'] ?></p>
-<div class="clearfix"></div>	
+<div class="col-md-12 col-lg-12 diary_comment_container">
+<div class="col-md-2 col-lg-2">
+	<img class="thumbnail" src="holder.js/64x64" alt="author">	
+	<small><?php echo $comment['author'] ?></small>
 </div>
- 
 
+<div class="col-lg-10 col-md-10 diary_comment">
+
+<i class="fa fa-caret-left diary_left_arrow fa-2"></i>
+<?php echo $comment['content'] ?>
+<br><br>
+<small><strong>	<?php echo date("F j, Y, g:i a",strtotime($comment['date'])); ?></strong></small>
+
+</div>
+<div class="clearfix"></div>
+ </div>
+<div class="clearfix"></div>
 <?php
 endforeach;
 ?>
