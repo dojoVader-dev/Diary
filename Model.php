136899e9ae67199ea5fb04ebc4825df6ac168d93
Okeowo <okeowoaderemi@gmail.com>
2014-06-20 12:08:35 +0100

    Recovered from Lost Heads

--- Model.php
+++ Model.php
@@ -32,6 +32,52 @@ class Model extends BaseModel {
 		);
 		return ipDb ()->insert ( "diary_blog", $saveData );
 	}
+	public function getPaginator($table, $currentPageIdx,$pageSize) {
+    	// let's fetch the total from the Database first
+    	/**
+		@todo hardcoded value, change later
+    	 */
+    	$pageSize=30;
+    	$recordCount = (int)ipDb ()->fetchValue ( sprintf ( "SELECT COUNT(*) from %s", ipTable ($table) ) );
+    	$totalPages =(int) ceil ( $recordCount / $pageSize);
+    	$currentPage = $currentPageIdx;
+    	if ($currentPage > $totalPages) {
+    		$currentPage = $totalPages;
+    	}
+    	$from = (abs($currentPage - 1)) * $pageSize;
+
+    	//Empty Result
+    	$pagination = new \Ip\Pagination\Pagination ( array (
+    			'data'=>$this->fetch($from, $pageSize),
+    			'currentPage' => $currentPage,
+    			'totalPages' => $totalPages,
+    			'pagerSize' => $pageSize
+    	) );
+    	return $pagination;
+
+
+
+    }
+
+	private function fetch($from, $count, $where = 1) {
+
+    	$sortField = 'id';
+    	// select ip_diary_blog.author,ip_diary_blog.date,ip_diary_blog.content,ip_diary_blog.id,title,ip_diary_blog.status,ip_diary_blog.category_id,dc.id as dcid ,dc.name 
+    	// from ip_diary_blog INNER JOIN ip_diary_category dc ON ip_diary_blog.category_id=dc.id
+
+    	$sql = "select ip_diary_blog.author,ip_diary_blog.date,ip_diary_blog.content,
+    	ip_diary_blog.id,title,ip_diary_blog.status,ip_diary_blog.category_id,dc.id as dcid ,dc.name 
+    	from ip_diary_blog INNER JOIN ip_diary_category dc ON ip_diary_blog.category_id=dc.id ORDER BY " . $sortField . "
+                LIMIT
+                $from, $count
+                ";
+
+                $result = ipDb ()->fetchAll ( $sql );
+
+
+    		return $result;
+    }
+
 
 
 	public function update() {
