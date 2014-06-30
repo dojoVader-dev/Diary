6ee145358192380c417cd01d533cf1dc2d129e00
Okeowo <okeowoaderemi@gmail.com>
2014-06-30 18:48:23 +0100

    Added the route name for ipRouteUrl

--- routes.php
+++ routes.php
@@ -4,6 +4,7 @@ $routes['article{/postid}'] = array(
     'plugin' => 'Diary',
     'controller' => 'SiteController',
     'action' => 'read',
+    'name'=>'article',
     'where' => array(
         'postid' => '\d+',
     )
