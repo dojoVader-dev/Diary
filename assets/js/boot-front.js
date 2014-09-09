501edc0a4df9f5e3d29a426d17f55421bf67082d
Okeowo <okeowoaderemi@gmail.com>
2014-09-09 18:38:21 +0100

    Removed DiaryController not even sure how this got here

--- assets/js/boot-front.js
+++ assets/js/boot-front.js
@@ -1,7 +1,7 @@
 /**
  * The Bootup for the Diary that loads the script
  */
-require(['dojo/parser'],function(DiaryController,parser){
+require(['dojo/parser'],function(parser){
 "use strict";
 	$('document').ready(function(){
 		document.body.className+=" claro";
