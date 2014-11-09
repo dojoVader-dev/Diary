6e5a9a0699bdc27dd8bfcdb844ca2aa689692c8e
Okeowo <okeowoaderemi@gmail.com>
2014-11-09 11:01:15 +0100

    Altered UI -> Changed UI and adding the Dojotoolkit MVC section

--- view/backend/ui.php
+++ view/backend/ui.php
@@ -1,32 +1,37 @@
 <?php
 if($articles):
 ?>
-
 <div class="ipModuleAdministrators ipsModuleAdministrators edit">
-    <div class="col-sm-3 col-lg-1 col-md-1 ui-diary-left">
+    <div class="col-sm-3 col-xs-3 col-lg-1 col-md-1 ui-diary-left">
         <ul class="nav nav-pills nav-stacked" role="tablist" style="max-width: 300px;">
-            <li role="presentation" class="active"><a href="#"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" border="0" class="img-circle img-responsive"></a></a></li>
-            <li role="presentation"><a href="#"><i class="fa fa-pencil-square-o fa-3x"></i><p>Articles</p></a>
+            <li class="active" role="presentation"><a href="#"><i class="fa fa-pencil-square-o fa-2x"></i><p>Articles</p></a>
             </li>
-            <li role="presentation"><a href="#"><i class="fa fa-comments-o fa-3x"></i><p>Comments</p></a></li>
+            <li role="presentation"><a href="#"><i class="fa fa-comments-o fa-2x"></i><p>Comments</p></a></li>
         </ul>
+    </div>
+    <div class="col-md-11 col-lg-11 col-sm-9 col-xs-9 killPadding" >
+        <div class="col-md-12 col-lg-12 ui-diary-topbar">
+            <div class="col-md-3 col-lg-3" style="height:inherit">
+                <div class="ui-diary-articlepane">
+                    <a href="#" class="pull-right"><i class="fa fa-plus-square-o "> &nbsp; Create</i></a>
+                </div>
+                <div class="ui-diary-articleList">
+                    <?php echo $articles;?>
+                </div>
+            </div>
+            <div class="col-lg-9 col-md-9" style="height:inherit">
+                <div class="ui-diary-richtext-pane">
+                    <h2 data-dojo-type='dojox/mvc/Output' id='DiaryTitle' data-dojo-props="value: at(DiaryModel, 'title')">
+                    ${this.value}
+                    </h2>
+
+                </div>
+                <div class="ui-diary-richtext"data-dojo-type="dojox/mvc/Output" data-dojo-props="value:at(DiaryModel,'content')">
+            ${this.value}
         </div>
-    <br /><br />
-	<div class="col-md-8 col-lg-8 col-sm-8 col-xs-8" >
-
-		<div class="diarybar">
-		<i class="fa fa-pencil pull-left"> <span data-dojo-type='dojox/mvc/Output' id='DiaryTitle' data-dojo-props="value: at(DiaryModel, 'title')"></span></i>
-		<!-- Right -->
-		<a href="${this.value}" id='EditLink' title="Edit" data-dojo-type="dojox/mvc/Output" data-dojo-props="value:at(DiaryModel,'id')"><i class="fa fa-2 fa-pencil-square-o pull-right"></i></a>
-		<div class="clearfix"></div>
-		</div>
-		<div class="diarytext" data-dojo-type="dojox/mvc/Output" data-dojo-props="value:at(DiaryModel,'content')">
-			${this.value}
-		</div>
-	</div>
-
-
-<?php
-endif;
-?>
-
+            </div>
+        </div>
+    </div>
+    <?php
+    endif;
+    ?>
