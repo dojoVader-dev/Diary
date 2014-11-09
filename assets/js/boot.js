944fa8bd6a984de411dc94d3d9489367b1a3d641
Okeowo <okeowoaderemi@gmail.com>
2014-11-09 09:31:01 +0100

    Altered UI -> Left Sidebar Complete

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -5,6 +5,8 @@ require(['Diary/DiaryController','dojo/parser'],function(DiaryController,parser)
 "use strict";
 	$('document').ready(function(){
 		document.body.className+=" claro";
+        //Fix the Height of left sidebar
+        $("div.ui-diary-left").height($('body').height());
 		parser.parse();
 	});
 });
--- view/backend/ui.php
+++ view/backend/ui.php
@@ -4,13 +4,12 @@ if($articles):
 
 <div class="ipModuleAdministrators ipsModuleAdministrators edit">
     <div class="col-sm-3 col-lg-1 col-md-1 ui-diary-left">
-        <!--Author Image-->
-        <div class="tab-content">
-        <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" border="0" class="img-circle img-responsive"></a>
-        </div>
-        <div class="tab-content">
-            
-        </div>
+        <ul class="nav nav-pills nav-stacked" role="tablist" style="max-width: 300px;">
+            <li role="presentation" class="active"><a href="#"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" border="0" class="img-circle img-responsive"></a></a></li>
+            <li role="presentation"><a href="#"><i class="fa fa-pencil-square-o fa-3x"></i><p>Articles</p></a>
+            </li>
+            <li role="presentation"><a href="#"><i class="fa fa-comments-o fa-3x"></i><p>Comments</p></a></li>
+        </ul>
         </div>
     <br /><br />
 	<div class="col-md-8 col-lg-8 col-sm-8 col-xs-8" >
